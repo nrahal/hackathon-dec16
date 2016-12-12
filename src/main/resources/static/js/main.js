1f325ee4a82f668addbb21bc946ce831d60517e9
Nacim Rahal <nrahal@expedia.com>
2016-12-12 11:03:08 +0100

    Add share link to itinerary page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,2 +1,2 @@
-var element = document.getElementById('hplogo');
-element.textContent = 'Hello World!';
+var html_fb_button = '<div class="col col1 align-right col-md-5 col-sm-5 zero-left-padding uppercase"><a id="tools-share-fb" href="#" class="zero-right-margin" data-control="modal" data-modal-id="receiptModal"><i class="icon icon-file"/>&nbsp;Share</a></div>';
+$('#tools-view').append( html_fb_button );
