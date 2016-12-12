f8ebb88e1bf6e8a890b10ab2f89e94792e4170b4
Nacim Rahal <nrahal@expedia.com>
2016-12-12 13:32:34 +0100

    Create facebook controller

--- springboot-backend/src/main/java/com/egencia/hackathon/controller/ShareController.java
+++ springboot-backend/src/main/java/com/egencia/hackathon/controller/ShareController.java
@@ -1,15 +1,22 @@
 package com.egencia.hackathon.controller;
 
+import com.egencia.hackathon.model.PublicationPageModel;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
 @RequestMapping(value = "/share", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
@@ -17,13 +24,24 @@ public class ShareController {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(ShareController.class);
 
-	@ResponseBody
-	@RequestMapping(value = "/trips", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public ResponseEntity<Void> process(@RequestBody(required = true) final TripMetaData metaData) {
+	@RequestMapping(value = "/facebook/{tripMetaDataBase64}", method = RequestMethod.GET)
+	public ModelAndView generateFacebookPage(@PathVariable String tripMetaDataBase64) throws IOException {
 
-		LOGGER.info("Sharing trip metadata : {}", metaData);
-		return new ResponseEntity<>(HttpStatus.OK);
+		String tripMetaDataJson = new String(Base64.getDecoder().decode(tripMetaDataBase64));
+		TripMetaData tripMetaData = new ObjectMapper().readValue(tripMetaDataJson, TripMetaData.class);
+		PublicationPageModel publicationPageModel = buildPublication(tripMetaData);
 
+		return new ModelAndView("facebook", "publication", publicationPageModel);
 	}
+
+	private PublicationPageModel buildPublication(TripMetaData tripMetaData) {
+		PublicationPageModel model = new PublicationPageModel();
+
+		model.setDestinationName(tripMetaData.getDestination());
+
+		return model;
+	}
+
+
 
 }
---
+++ springboot-backend/src/main/java/com/egencia/hackathon/model/PublicationPageModel.java
@@ -0,0 +1,24 @@
+package com.egencia.hackathon.model;
+
+public class PublicationPageModel {
+
+    private String destinationName;
+    private String imageUrl;
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
