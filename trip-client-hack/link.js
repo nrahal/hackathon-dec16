f0b0d10b1d89f336d972482324e30e9d951e1cf7
Remy CREPIN <rcrepin@PARC02Q82LMG8WL.sea.corp.expecn.com>
2016-12-12 20:25:59 +0100

    Remove trip-client-hack

--- trip-client-hack/link.js
+++
@@ -1,5 +0,0 @@
-javascript:(function() {
-    var script = document.createElement('script');
-    script.src = 'https://54.171.123.75/nr/static/js/main.js';
-    document.head.appendChild(script);
-})();
