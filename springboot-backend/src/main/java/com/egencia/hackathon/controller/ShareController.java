bf065efeadd54da61bbaa2e0e7b2a8633ed15943
Grégoire Cacheux <gcacheux@egencia.com>
2016-12-12 13:12:40 +0100

    EC2 call backend

--- springboot-backend/src/main/java/com/egencia/hackathon/controller/ShareController.java
+++ springboot-backend/src/main/java/com/egencia/hackathon/controller/ShareController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/share", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
@@ -24,6 +24,15 @@ public class ShareController {
 		LOGGER.info("Sharing trip metadata : {}", metaData);
 		return new ResponseEntity<>(HttpStatus.OK);
 
+	}
+
+	@RequestMapping("/fb/{payload}/{bust}")
+	public String greeting(
+                           @RequestParam(value="payload", required=false, defaultValue="World") String payload,
+                           @RequestParam(value="bust", required=false) String bust,
+                           Model model) {
+		model.addAttribute("currentUrl", "http://54.171.123.75/gc/share/fb");
+		return "fb";
 	}
 
 }
